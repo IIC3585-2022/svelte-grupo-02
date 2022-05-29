ec0fdcb734a8f0f9398794881f2c2b7070ff8156
meretamal <meretamal@uc.cl>
2022-05-29 18:22:38 -0400

    Refactor code with event modifiers

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -1,18 +1,13 @@
 <script>
   import { push } from 'svelte-spa-router';
   import { username } from '../stores/session';
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    push('/calls');
-  };
 </script>
 
 <div
   class="container h-full is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
 >
   <h1 class="has-text-centered is-size-1">Ingresa tu nombre de usuario</h1>
-  <form on:submit={handleSubmit} class="w-80 my-5">
+  <form on:submit|preventDefault={() => push('/calls')} class="w-80 my-5">
     <div class="field">
       <input
         type="text"
